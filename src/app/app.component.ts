23565260987db51d61117bdebe085b285002b53e
Rahul Ranjan <ranjan.rahul326@gmail.com>
2017-05-04 11:58:39 +0530

    Reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
-@Component({
+import { FormGroup, FormControl } from '@angular/forms';
+ @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Model Driven- Reactive Forms';
+   userForm =  new FormGroup({
+     name: new FormControl(),
+     email: new FormControl(),
+     contact: new FormControl(),
+     address: new FormGroup({
+         street: new FormControl(),
+         city: new FormControl(),
+         pin: new FormControl()
+     })
+   });
 }
