b7de79bd024e325c2a00f820c02b9c0742614a62
Rahul Ranjan <ranjan.rahul326@gmail.com>
2017-05-04 12:31:44 +0530

    submit method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
- @Component({
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Model Driven- Reactive Forms';
-   userForm =  new FormGroup({
-     name: new FormControl(),
-     email: new FormControl(),
-     contact: new FormControl(),
-     address: new FormGroup({
-         street: new FormControl(),
-         city: new FormControl(),
-         pin: new FormControl()
-     })
-   });
+  userForm = new FormGroup({
+    name: new FormControl('Rahul Ranjan'),
+    email: new FormControl(),
+    contact: new FormControl(),
+    address: new FormGroup({
+      street: new FormControl(),
+      city: new FormControl(),
+      pin: new FormControl()
+    })
+  });
+  onSubmit() {
+    console.log('It works!');
+    console.log(this.userForm.value);
+  }
 }
